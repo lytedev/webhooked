aba7126fa74a338a7599a21d7ce7586ea1f1c441
Daniel Flanagan <daniel@lytedev.io>
2017-05-10 15:14:17 -0500

    Fix example script including old scripts

--- scripts/example-lytedev-hugo-site.bash
+++ scripts/example-lytedev-hugo-site.bash
@@ -6,13 +6,10 @@ set -e
 # Get the source directory of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Define the proper directory in ../env_scripts/dirs.bash
-source "$DIR/../env_scripts/dirs.bash"
-if [ -z "$LYTEDEV_HUGO_DIR" ]; then
-	echo "\$LYTEDEV_HUGO_DIR is not set. Does $DIR/../env_scripts/dirs.bash exist?"
-	exit 1
-fi
+# Define other variables
+LYTEDEV_HUGO_DIR="$HOME/sites/lytedev"
 
+# Go!
 echo "Moving to directory $LYTEDEV_HUGO_DIR"
 pushd "$LYTEDEV_HUGO_DIR"
 git pull origin source
